53595b1a22dcdacae11b5760922282b98c1a461a
Faddragon <109700345+Faddragon@users.noreply.github.com>
2025-07-28 12:55:35 -0300

    Update dashboard_cirurgias_app_v3.py

--- dashboard_cirurgias_app_v3.py
+++ dashboard_cirurgias_app_v3.py
@@ -77,6 +77,60 @@ if pagina == "📊 Visão Geral":
     df_est.columns = ["Tipo de Cirurgia", "N", "Mínimo (h)", "Máximo (h)", "Média (h)", "Desvio Padrão (h)"]
     st.dataframe(df_est)
 
+
+    # 🕒 Duração por grupo mestre
+    st.subheader("🕒 Duração Cirúrgica por Grupo Patológico")
+    df_tempo = df.groupby("GRUPO_MESTRE")["DURACAO_HORAS"].agg(["min", "max", "mean"]).reset_index().round(2)
+    df_tempo.columns = ["Grupo Patológico", "Mínimo (h)", "Máximo (h)", "Média (h)"]
+    fig_duracao = px.bar(df_tempo, x="Média (h)", y="Grupo Patológico", orientation="h", hover_data=["Mínimo (h)", "Máximo (h)"], color="Grupo Patológico")
+    fig_duracao.update_layout(template="simple_white", showlegend=False, height=500)
+    st.plotly_chart(fig_duracao, use_container_width=True)
+
+    # 💉 Anestesia LOCAL por mês
+    st.subheader("💉 Casos com Anestesia LOCAL por Mês")
+    df_local = df[df['ANEST'] == 'LOCAL']
+    df_local_mes = df_local.groupby('ANO_MES').size().reset_index(name='Quantidade')
+    fig_local_mes = px.bar(
+        df_local_mes,
+        x='ANO_MES',
+        y='Quantidade',
+        text='Quantidade',
+        labels={'ANO_MES': 'Mês', 'Quantidade': 'Número de Casos'}
+    )
+    fig_local_mes.update_layout(
+        xaxis={'categoryorder': 'category ascending'},
+        template='simple_white',
+        height=400
+    )
+    st.plotly_chart(fig_local_mes, use_container_width=True)
+
+      # 🧪 Anestesia LOCAL (Excluidas as traqueostomias)
+    st.subheader("🧪 Casos com Anestesia LOCAL sem Traqueostomia por Mês")
+    df_local_sem_traq = df[
+        (df['ANEST'] == 'LOCAL') &
+        (~df['CIRURGIA_GRUPO'].str.contains('TRAQUEOSTOMIA', case=False, na=False))
+    ]
+    df_local_sem_traq_mes = df_local_sem_traq.groupby('ANO_MES').size().reset_index(name='Quantidade')
+    fig_sem_traq_mes = px.bar(
+        df_local_sem_traq_mes,
+        x='ANO_MES',
+        y='Quantidade',
+        text='Quantidade',
+        labels={'ANO_MES': 'Mês', 'Quantidade': 'Número de Casos'}
+    )
+    # cor personalizada (por exemplo, seagreen)
+    fig_sem_traq_mes.update_traces(marker_color='seagreen')
+    fig_sem_traq_mes.update_layout(
+        xaxis={'categoryorder': 'category ascending'},
+        template='simple_white',
+        height=400
+    )
+    st.plotly_chart(fig_sem_traq_mes, use_container_width=True)
+
+
+
+    
+
     st.subheader("🔍 Buscar Paciente por Número MV")
     mv_input = st.text_input("Digite o número MV:")
     if mv_input:
